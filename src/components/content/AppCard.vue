d89e019df695e371af25bba3b1ff97c4fe4c94a1
Eric Zawadski <ericz@live.ca>
2021-06-26 18:35:43 -0500

    Added content components

---
+++ src/components/content/AppCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <ion-card>
+    <ion-card-content>
+      <ion-img :src="imgSrc"></ion-img>
+      <ion-card-title>{{ title }}</ion-card-title>
+      <ion-text>{{ text }}</ion-text>
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { IonCard, IonCardContent, IonCardTitle, IonText } from '@ionic/vue';
+
+export default defineComponent({
+  name: 'AppHeading',
+  components: { IonCard, IonCardContent, IonCardTitle, IonText },
+  props: {
+    title: String,
+    imgSrc: String,
+    text: String
+  }
+});
+</script>
+
+<style></style>
